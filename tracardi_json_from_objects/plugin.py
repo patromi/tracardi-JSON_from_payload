cfacfc6843483056035426d55fda123cd9d85f04
risto <risto.kowaczewski@gmail.com>
2021-11-30 22:59:05 +0100

    Now converts to json.

--- tracardi_json_from_objects/plugin.py
+++ tracardi_json_from_objects/plugin.py
@@ -1,21 +1,27 @@
+import json
+
 from tracardi_plugin_sdk.action_runner import ActionRunner
-from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData
+from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData, Form, FormGroup, FormField, FormComponent
 from tracardi_plugin_sdk.domain.result import Result
 from tracardi_json_from_objects.model.models import Configuration
-from tracardi_dot_notation.dot_accessor import DotAccessor
-from tracardi_dot_notation.dot_template import DotTemplate
+
+
+def validate(config: dict):
+    return Configuration(**config)
 
 
 class ConvertAction(ActionRunner):
 
     def __init__(self, **kwargs):
-        self.config = Configuration(**kwargs)
+        self.config = validate(kwargs)
 
     async def run(self, payload):
         dot = self._get_dot_accessor(payload)
         path = dot[self.config.to_json]
 
-        return Result(port="payload", value={"json": path})
+        result = json.dumps(dict(path), default=str)
+
+        return Result(port="payload", value={"json": result})
 
 
 def register() -> Plugin:
@@ -29,15 +35,30 @@ def register() -> Plugin:
             version='0.1',
             license="MIT",
             author="Patryk Migaj",
-            init={}
+            init={
+                "to_json": None
+            },
+            form=Form(groups=[
+                FormGroup(
+                    fields=[
+                        FormField(
+                            id="to_json",
+                            name="Path to data",
+                            description="Path to data to be serialized to JSON. "
+                                        "E.g. profile@stats.counters.boughtProducts",
+                            component=FormComponent(type="dotPath", props={"label": "Field path"})
+                        )
+                    ]
+                )
+            ]),
         ),
         metadata=MetaData(
-            name='tracardi-json-from-objects',
+            name='To JSON',
             desc='The purpose of this plugin is convert objects to JSON',
             type='flowNode',
             width=200,
             height=100,
-            icon='icon',
+            icon='json',
             group=["General"]
         )
     )
